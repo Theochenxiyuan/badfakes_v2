5b2e5465229fc7d67169b88751d2749bb001b792
Theochenxiyuan <theochenxiyuan@outlook.com>
2022-09-10 20:01:19 +1000

    nothing

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
 const modalBegin = document.getElementById('modal-begin');
-
 const modalQuestion = document.getElementById('modal-question');
-
 const close = document.querySelector('#modal-question .btn-close');
-
 const start = document.querySelector('.start');
-
 const topBar = document.querySelector('.top');
-
 const botBar = document.querySelector('.bot');
-
 const main = document.querySelector('.main-content');
-
 const next = document.querySelector('.next');
-
 const buttons = document.querySelector('.buttons');
-
 const show = document.querySelector('.show');
-
 const titleQ = document.querySelector('#modal-question .modal-title');
-
 const bodyQ = document.querySelector('#modal-question .modal-body');
-
 const titleS = document.querySelector('#modal-begin .modal-title');
-
 const bodyS = document.querySelector('#modal-begin .modal-body');
-
 const end = document.querySelector('.end');
-
 const restart = document.querySelector('.restart');
-
 const correctSound = new Audio('sound/correct.mp3');
 const wrongSound = new Audio('sound/wrong.mp3');
 const winSound = new Audio('sound/WIN.mp3');
@@ -37,7 +21,6 @@ const loseSound = new Audio('sound/LOSE.mp3');
 
 function updateQuestion() {
   topBar.innerHTML = `${questionNumber} of ${totalNumber}`;
-
   const question = questions[questionNumber - 1];
 
   if (question.type === 'img') {
