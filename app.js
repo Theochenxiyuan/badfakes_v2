a25d481079dafeced5aa9fdc4c4c89f63d3c679f
Theochenxiyuan <theochenxiyuan@outlook.com>
2022-09-10 16:34:48 +1000

    badfakesV2-1

---
+++ app.js
@@ -0,0 +1,137 @@
+const modalBegin = document.getElementById('modal-begin');
+
+const modalQuestion = document.getElementById('modal-question');
+
+const close = document.querySelector('#modal-question .btn-close');
+
+const start = document.querySelector('.start');
+
+const topBar = document.querySelector('.top');
+
+const botBar = document.querySelector('.bot');
+
+const main = document.querySelector('.main-content');
+
+const next = document.querySelector('.next');
+
+const buttons = document.querySelector('.buttons');
+
+const show = document.querySelector('.show');
+
+const titleQ = document.querySelector('#modal-question .modal-title');
+
+const bodyQ = document.querySelector('#modal-question .modal-body');
+
+const titleS = document.querySelector('#modal-begin .modal-title');
+
+const bodyS = document.querySelector('#modal-begin .modal-body');
+
+const end = document.querySelector('.end');
+
+const correctSound = new Audio('sound/correct.mp3');
+const wrongSound = new Audio('sound/wrong.mp3');
+const winSound = new Audio('sound/WIN.mp3');
+const loseSound = new Audio('sound/LOSE.mp3');
+
+function updateQuestion() {
+  topBar.innerHTML = `${questionNumber} of ${totalNumber}`;
+
+  const question = questions[questionNumber - 1];
+
+  if (question.type === 'img') {
+    main.innerHTML = `<img src="${question.src}" alt="image">`;
+  } else if (question.type === 'vid') {
+    main.innerHTML = `<video controls loop controlsList="nodownload"><source src="${question.src}" type="video/mp4"></video>`;
+  }
+}
+
+function updateCorrect() {
+  correctRate = Math.round((correct / totalNumber) * 100);
+
+  botBar.innerHTML = `Correct: ${Math.round(
+    (correct / questionNumber) * 100
+  )}%`;
+}
+
+function closeModal() {
+  modalQuestion.classList.remove('d-block');
+}
+window.onload = () => {
+  modalBegin.style.display = 'block';
+};
+
+close.addEventListener('click', () => {
+  closeModal();
+});
+
+show.addEventListener('click', () => {
+  modalQuestion.classList.add('d-block');
+});
+
+next.addEventListener('click', () => {
+  questionNumber += 1;
+  buttons.classList.remove('d-none');
+  show.classList.add('d-none');
+  updateQuestion();
+  closeModal();
+});
+
+start.addEventListener('click', () => {
+  modalBegin.style.display = 'none';
+  questionNumber = 1;
+  updateQuestion();
+  updateCorrect();
+});
+
+buttons.addEventListener('click', (e) => {
+  const question = questions[questionNumber - 1];
+  const choice = e.target.classList.contains('real') ? 'real' : 'fake';
+
+  if (choice === question.ans) {
+    correctSound.play();
+    correct += 1;
+    titleQ.innerText = 'Correct!';
+    titleQ.classList.add('text-success');
+    titleQ.classList.remove('text-danger');
+  } else {
+    wrongSound.play();
+    titleQ.innerText = 'Incorrect!';
+    titleQ.classList.add('text-danger');
+    titleQ.classList.remove('text-success');
+  }
+
+  if (question.ans === 'real') {
+    bodyQ.innerText = 'This is NOT a Deepfake. ';
+  } else if (question.ans === 'fake') {
+    bodyQ.innerText = `This is a Deepfake. ${question.exp}`;
+  }
+
+  if (questionNumber === totalNumber) {
+    end.classList.remove('d-none');
+    next.classList.add('d-none');
+  }
+
+  buttons.classList.add('d-none');
+  show.classList.remove('d-none');
+  modalQuestion.classList.add('d-block');
+  updateCorrect();
+});
+
+end.addEventListener('click', () => {
+  modalQuestion.classList.remove('d-block');
+  modalBegin.classList.add('d-block');
+  start.classList.add('d-none');
+  titleS.innerText = `You got ${correctRate}% of the answers correct.`;
+
+  if (correctRate < cutOff) {
+    loseSound.play();
+    titleS.classList.add('text-danger');
+    bodyS.innerText =
+      'You have failed this test. Your ability to identify Deepfakes still needs to be improved. ';
+  } else {
+    winSound.play();
+    titleS.classList.add('text-success');
+    bodyS.innerText =
+      'You have passed this test. Your ability to identify Deepfakes is good enough! ';
+  }
+});
---
+++ question.js
@@ -0,0 +1,63 @@
+const totalNumber = 20;
+let questionNumber = 0;
+let questions = [];
+let correct = 0;
+let correctRate = 0;
+const cutOff = 60;
+const explanations = [
+  'You can see his face has a different skin colour and texture compared to his forehead and neck.',
+  'You can see there are some unnatural shadows around his nose and left eye.',
+  'You can see a clear boundary on her cheek.',
+  "Judging by his cloth and hair, the person is most likely facing at an orange-ish lighting. However, you can't see any orange tinge or reflection on his face skin. ",
+  'While it is not guaranteed, keep in mind that the non-stereotypical gender presentation can be a result of face swapping between a man and a woman.',
+  'While there is no clear boundary, the texture on his face is slightly different to the top of his forehead. Additionally, his nose looks like it is printed on a surface instead of being a solid body. ',
+  "It's not hard to tell because there are too many unnatural movements. Some weird artifacts also appear near his mouth. ",
+  "This one is hard but if you pay attention, you can see, unlike his left eye, his right eye isn't even fully opened for the most part of the video as supposed. You may also notice some inconsistency of skin colour right under his forehead. ",
+  'You can see some small artifacts keeps showing up and his chin moves a bit unnaturally. ',
+  'You can see the colour of her lips is inconsistent. Additionally, her face skin is weirdly bright and reflective compared to her neck and hair. ',
+];
+
+class Question {
+  constructor(type, ans, num, exp = 'No explanation.') {
+    this.type = type;
+    this.ans = ans;
+    this.num = num;
+    this.exp = explanations[exp];
+    const ext = this.type === 'img' ? 'jpg' : 'mp4';
+    this.src = `${this.type}/${this.ans}/${this.num}.${ext}`;
+  }
+}
+
+let orders = [];
+
+while (orders.length < totalNumber) {
+  let random = (Math.random() * (totalNumber - 1)).toFixed();
+  random = Number(random);
+  if (!orders.includes(random)) {
+    orders.push(random);
+  }
+}
+
+questions[orders[0]] = new Question('img', 'real', 1);
+questions[orders[1]] = new Question('img', 'real', 2);
+questions[orders[2]] = new Question('img', 'real', 3);
+questions[orders[3]] = new Question('img', 'real', 4);
+questions[orders[4]] = new Question('img', 'real', 5);
+questions[orders[5]] = new Question('img', 'real', 6);
+
+questions[orders[6]] = new Question('img', 'fake', 1, 0);
+questions[orders[7]] = new Question('img', 'fake', 2, 1);
+questions[orders[8]] = new Question('img', 'fake', 3, 2);
+questions[orders[9]] = new Question('img', 'fake', 4, 3);
+questions[orders[10]] = new Question('img', 'fake', 5, 4);
+questions[orders[11]] = new Question('img', 'fake', 6, 5);
+
+questions[orders[12]] = new Question('vid', 'real', 1);
+questions[orders[13]] = new Question('vid', 'real', 2);
+questions[orders[14]] = new Question('vid', 'real', 3);
+questions[orders[15]] = new Question('vid', 'real', 4);
+
+questions[orders[16]] = new Question('vid', 'fake', 1, 6);
+questions[orders[17]] = new Question('vid', 'fake', 2, 7);
+questions[orders[18]] = new Question('vid', 'fake', 3, 8);
+questions[orders[19]] = new Question('vid', 'fake', 4, 9);
